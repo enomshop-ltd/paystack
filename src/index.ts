aa0100687df0320e8340e550ed78d6c85445479d
Nelly #1 <122103189+enomshop-ltd@users.noreply.github.com>
2026-04-06 02:33:44 +0300

    Import and export PaystackProvider in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 // src/index.ts
+import PaystackProvider from "./providers/paystack";
+
+export default PaystackProvider;
 export { PaystackPaymentProcessorConfig as PluginOptions } from "./services/paystack-payment-processor";
